f9e870a6f772c5e746e0468d59533650d9e8776f
Cairo Stewart <stewartcairo@gmail.com>
2016-02-09 11:50:28 -0800

    everything

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ var morgan = require('morgan');
 app.use(express.static('public'));
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/views/index.html');
-}).listen(8000)
+}).listen( process.env.PORT || 8000)
