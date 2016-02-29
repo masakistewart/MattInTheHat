baf105478d96590c2c0b1b94b3868f2270b3d35d
Cairo Stewart <stewartcairo@gmail.com>
2016-02-28 17:05:29 -0800

    everythin

--- public/js/style.js
+++ public/js/style.js
@@ -1,5 +1,23 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '737162956417049',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 $(document).ready(function(){
  $('.parallax').parallax();
+ $(".button-collapse").sideNav();
+ $('.modal-trigger').leanModal();
  SC.initialize({
     client_id: 'dabf914acbf33b065ea1a28fd92fc296',
     redirect_uri: 'http://localhost:8000'
@@ -9,5 +27,9 @@ $(document).ready(function(){
  	console.log(data);
  })
 
- SC.oEmbed('http://soundcloud.com/m_i_t_h', {element: document.getElementById('sound')})
+ SC.oEmbed('http://soundcloud.com/m_i_t_h', {
+ 	element: document.getElementById('soundCloudTarget'),
+ 	minwidth: "100%",
+ 	maxheight: "400"
+ })
 });
